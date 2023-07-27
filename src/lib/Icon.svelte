a78299dc3ae6bb2cd14dd79e1c64c82daa962247
Shinichi Okada <okada.shin@gmail.com>
2023-07-27 20:40:24 +0200

    feat: change to two files

---
+++ src/lib/Icon.svelte
@@ -0,0 +1,12 @@
+<script>
+import icons from './icons.js'
+  export let name;
+  export let width = "24";
+  export let height = "24";
+  let displayIcon = icons[name]
+</script>
+<svg
+class={$$props.class}
+{width}
+{height}
+viewBox="0 0 {displayIcon.box} {displayIcon.box}">{@html displayIcon.svg}</svg>
